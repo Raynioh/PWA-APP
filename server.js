1d0e6364a28869c1f71df1d7416b8196b8414274
Raynioh <roko.skoblar@gmail.com>
2025-01-30 22:24:56 +0100

    novi server

--- server.js
+++ server.js
@@ -6,20 +6,6 @@ const externalUrl = process.env.RENDER_EXTERNAL_URL;
 const port =
   externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 4080;
 
-if (externalUrl) {
-  const hostname = "0.0.0.0";
-  app.listen(port, hostname, () => {
-    console.log(`Server locally running at http://${hostname}:${port}/ and from
-    outside on ${externalUrl}`);
-  });
-} else {
-  http
-    .createServer(app)
-    .listen(port, function () {
-      console.log(`Server running at http://localhost:${port}/`);
-    });
-}
-
 app.use(express.static(path.join(__dirname, "assets")));
 
 app.get("/", (req, res) => {
@@ -30,6 +16,14 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(4, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+if (externalUrl) {
+  const hostname = "0.0.0.0";
+  app.listen(port, hostname, () => {
+    console.log(`Server locally running at http://${hostname}:${port}/ and from
+    outside on ${externalUrl}`);
+  });
+} else {
+  app.listen(port, function () {
+    console.log(`Server running at http://localhost:${port}/`);
+  });
+}
